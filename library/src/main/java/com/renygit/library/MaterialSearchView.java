4e0cc4990b5e284e15def0e8d62ba810f40ba826
reny <729608351@qq.com>
2017-09-01 09:40:34 +0800

    ...

--- library/src/main/java/com/renygit/library/MaterialSearchView.java
+++ library/src/main/java/com/renygit/library/MaterialSearchView.java
@@ -146,7 +146,8 @@ public class MaterialSearchView extends FrameLayout implements Filter.FilterList
     }
 
     private void initiateView() {
-        mSearchLayout = LayoutInflater.from(mContext).inflate(R.layout.search_view, this, true);
+        LayoutInflater.from(mContext).inflate(R.layout.search_view, this, true);
+        mSearchLayout = findViewById(R.id.rootView);
 
         mSearchTopBar = mSearchLayout.findViewById(R.id.search_top_bar);
         mSuggestionsRecyclerView = (RecyclerView) mSearchLayout.findViewById(R.id.suggestion_list);
